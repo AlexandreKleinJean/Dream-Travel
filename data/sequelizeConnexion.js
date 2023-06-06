7bc813de9410f3d3ec85df013f3bbdd0182ed200
AlexandreKleinJean <alexandre.klein.jean@gmail.com>
2023-06-06 15:34:57 +0200

    update 06/06/2023 afternoon

--- data/sequelizeConnexion.js
+++ data/sequelizeConnexion.js
@@ -10,12 +10,15 @@ function getConnexion() {
     });
 
 
-	/*try {
+	/*-------Vérifier connection sequelize-------------------
+    try {
          await sequelize.authenticate();
          console.log('Connection has been established successfully.');
      } catch (error) {
          console.error('Unable to connect to the database:', error);
-     }*/
+     }
+     */
+    
      return sequelize;
 }
 
